8f3d66acec1495bcca25b083a11d7a3ca9fa15f2
bian.kai <bian.kai@ruanyun.net>
2019-05-20 15:17:36 +0800

    添加动态修改主题颜色示例

--- src/components/ReactApp.jsx
+++ src/components/ReactApp.jsx
@@ -1,7 +1,40 @@
 import React, { Fragment } from 'React';
-import { Carousel, Button } from 'antd';
+import { Carousel, Button, message } from 'antd';
 import './ReactApp.css';
 import style from './ReactApp.less';
+
+/**
+ * 加载脚本
+ * @param src
+ * @return {Promise}
+ */
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+/**
+ * 加载样式
+ * @param href
+ * @return {Promise}
+ */
+function loadLink(href) {
+  return new Promise((resolve, reject) => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet/less';
+    link.type = 'text/css';
+    link.href = href;
+    link.onerror = reject;
+    document.head.appendChild(link);
+    resolve();
+  });
+}
 
 /**
  * 无 mock-server 响应时使用
@@ -46,6 +79,8 @@ class ReactApp extends React.Component {
    */
   constructor(props) {
     super(props);
+
+    this.lessLoaded = false;
 
     this.state = {
       msg: '使用 Ant Design { Carousel }',
@@ -95,6 +130,43 @@ class ReactApp extends React.Component {
   };
 
   /**
+   * 点击切换颜色
+   */
+  handleColorChange = () => {
+    /**
+     * 改变颜色
+     */
+    const changeColor = () => {
+      window.less
+        .modifyVars({
+          '@primary-color': '#F5222D',
+          '@border-radius-base': '2px',
+        })
+        .then(() => {
+          message.success('修改主要颜色成功');
+        });
+    };
+
+    const scriptUrl = 'https://alex86gbk.github.io/js-multi-seed/Content/less/less.min.js';
+    const lessUrl = 'https://alex86gbk.github.io/js-multi-seed/Content/less/color.less';
+
+    if (this.lessLoaded) {
+      changeColor();
+    } else {
+      window.less = {
+        async: true,
+        javascriptEnabled: true,
+      };
+      loadLink(lessUrl).then(() => {
+        loadScript(scriptUrl).then(() => {
+          this.lessLoaded = true;
+          changeColor();
+        });
+      });
+    }
+  };
+
+  /**
    * 渲染
    * @return {XML}
    */
@@ -116,6 +188,12 @@ class ReactApp extends React.Component {
           <Button type="danger">Danger</Button>
         </div>
         <p>全局主色：primary-color（默认值：#1890ff）、边框圆角：border-radius-base（默认值：4px）</p>
+        <Button onClick={this.handleColorChange}>动态修改主要颜色</Button>
+        <div style={{ marginTop: '15px' }}>
+          <span style={{ backgroundColor: '#1DA57A', width: '70px', height: '20px', display: 'inline-block' }}>#1DA57A</span>
+          &nbsp;======&gt;&nbsp;
+          <span style={{ backgroundColor: '#F5222D', width: '70px', height: '20px', display: 'inline-block' }}>#F5222D</span>
+        </div>
       </Fragment>
     );
   }
